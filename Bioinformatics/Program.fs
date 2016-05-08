c413e8b0c1ecd0e6d83fb4a77c1426e86a068dbf
Chaaru Dingankar <chaaru.dingankar@DevLoaner.local>
2016-05-08 12:03:59 -0700

    Committing a bunch of junk, trying to get gitignore to untrack already committed files.

--- Bioinformatics/Program.fs
+++ Bioinformatics/Program.fs
@@ -1,15 +1,22 @@
 ﻿open Bioinformatics.Alignment
+open Bioinformatics.Grammar
 
 [<EntryPoint>]                    
 let main argv = 
 
-    let p : NWParams = 
-        {
-            d = 5
-            s = Map.ofList [(('a', 'b'), 1); (('b', 'a'), 1); (('a', 'a'), 10); (('b', 'b'), 10)]
-        }
+//    let p : AlignmentParams = 
+//        {
+//            d = 5
+//            s = Map.ofList [(('a', 'b'), 1); (('b', 'a'), 1); (('a', 'a'), 10); (('b', 'b'), 10)]
+//        }
+//
+//    let x = NeedlemanWunsch p "abbababbabababba" "abbabababa"
+//    printfn "%A" x
 
-    let x = NeedlemanWunsch p "abbababbabababba" "abbabababa"
-    printfn "%A" x
+    printfn "hello..."
+    printfn "%A" (parseSomething())
+//    printfn "%A" (getDerivation())
+    
+    printfn "goodbye"
 
     0
