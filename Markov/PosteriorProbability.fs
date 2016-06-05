2fc69e7edbbfa11963792bdb2a189eed251aa33d
Chaaru Dingankar <chaaru.dingankar@gmail.com>
2016-06-05 10:47:04 -0700

    posterior decoding for sequences.

--- Markov/PosteriorProbability.fs
+++ Markov/PosteriorProbability.fs
@@ -4,6 +4,9 @@ open Forward
 open Backward
 open DPUtils
 
+let posteriorProbability prob fwdScore bwdScore = 
+    fwdScore * bwdScore / prob
+
 let findPosteriorProbabilityOfState (state: 'State) (index: int) startState hmm (observations: 'Emission list) =
     let forwardResult = forward startState hmm observations
     let forwardScore = 
@@ -21,6 +24,31 @@ let findPosteriorProbabilityOfState (state: 'State) (index: int) startState hmm 
         |> fun cell -> cell.score
 
     //forward probability should equal backward probability
-    forwardScore * backwardScore / forwardResult.probability
+    posteriorProbability forwardResult.probability forwardScore backwardScore 
 
-//TODO posterior decoding of sequences
+let posteriorDecoding startState hmm observations = 
+    let fwdResult = forward startState hmm observations
+    let fwdTable = fwdResult.table
+    let bwdResult = backward startState hmm observations
+    let bwdTable = bwdResult.table
+
+    let prob = fwdResult.probability
+    let posterior = posteriorProbability prob
+
+    let n = numColumns fwdTable
+
+    [0 .. (n-1)]
+    |> List.fold (fun acc i ->
+        let fwdCol = getColumnFromTable i fwdTable
+        let bwdCol = getColumnFromTable i bwdTable
+        let combinedColumn = Array.zip fwdCol bwdCol
+        let posteriorState = 
+            combinedColumn
+            |> Array.maxBy (fun (fwdCell, bwdCell) ->
+                //forward cell state should equal backward cell state
+                posterior fwdCell.score bwdCell.score)
+            |> fst
+            |> fun cell -> cell.state
+        posteriorState :: acc) []
+
+// TODO result of the posterior probability applied with a function G(i|x)
